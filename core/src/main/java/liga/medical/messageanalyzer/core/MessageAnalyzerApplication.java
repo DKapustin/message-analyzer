8d290250193dccd02149e6431004d486558caf55
DKapustin <32310510+DKapustin@users.noreply.github.com>
2022-10-05 20:16:35 +0300

    fix

--- core/src/main/java/liga/medical/messageanalyzer/core/MessageAnalyzerApplication.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/MessageAnalyzerApplication.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"liga.medical.medicalmonitoring", "liga.medical.common.service"})
 public class MessageAnalyzerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MedicalMonitoringApplication.class, args);
+        SpringApplication.run(MessageAnalyzerApplication.class, args);
     }
 }
